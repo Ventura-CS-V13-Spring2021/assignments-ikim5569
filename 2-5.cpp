95d7a4fd242cadb400ef254c6c6dd1a71e70aafb
Irene Kim <ikim5569@gmail.com>
2021-03-16 06:00:54 +0000

    2-5.cpp

--- 2-5.cpp
+++ 2-5.cpp
@@ -1,1 +1,29 @@
+#include <iostream> 
+using namespace std; 
 
+int main(){
+  int num, i, num1, num2; 
+
+  cout << "Input 2 numbers: "<< endl; 
+
+  cin >> num1 >> num2; 
+
+  while (num1 < 0 || num2 > 100){
+    cout << "Input 2 numbers: "<< endl; 
+    cin >> num1 >> num2; 
+  }
+
+  for (num = num1; num <= num2; num++){
+    for (i = 2; i <= num; i++){
+      if (num % i == 0){
+        break; 
+      }
+    }
+    if (i == num){
+      cout << num << " is prime" << endl; 
+    }
+
+    
+  }
+  
+} 
