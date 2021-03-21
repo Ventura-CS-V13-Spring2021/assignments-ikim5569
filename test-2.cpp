6081fce81fbd561e093c773892c60f324c78f57e
Irene Kim <ikim5569@gmail.com>
2021-03-21 21:59:28 +0000

    test 2 first

---
+++ test-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+  int num1, num2, num3; 
+
+  cout << "Please input three values between and including numbers 0 and 99! : " << endl; 
+
+  cin >> num1 >> num2 >> num3; 
+
+}
