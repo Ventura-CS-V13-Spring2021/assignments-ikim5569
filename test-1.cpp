5d0e186a960cf74fa0a9bb55830a79080cbefcc3
Irene Kim <ikim5569@gmail.com>
2021-03-21 21:45:04 +0000

    proof

--- test-1.cpp
+++ test-1.cpp
@@ -6,8 +6,24 @@
 using namespace std; 
 
 int main(){
-  int num; 
+  int num, previousNumber; 
 
 
   srand(time(0)); 
+
+  previousNumber = rand() % 100; 
+
+
+  for (int i = 1; i < 5; i++) {
+    num = rand()%100; 
+    cout << num << endl; 
+
+    if (num > previousNumber) {
+      cout << num << " is greater than " << previousNumber << endl; 
+    }
+
+    previousNumber = num; 
+
+  }
 }
+
