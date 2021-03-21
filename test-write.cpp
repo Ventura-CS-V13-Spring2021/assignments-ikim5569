e53a77235ad3c2d0ea77f43fddb9dc56367e7b22
Irene Kim <ikim5569@gmail.com>
2021-03-21 23:05:37 +0000

    test-write second

--- test-write.cpp
+++ test-write.cpp
@@ -5,9 +5,34 @@
 using namespace std; 
 
 int maint(){
+
+  int num, ID; 
+  string name, department; 
+  double salary; 
+
   ofstream write; 
 
   write.open("employee.txt"); 
+
+  cout << "How many employees? " << endl; 
+  cin >> num; 
+
+  
+
+  for (int i = 0; i < num; i++){
+    cout << "Please enter the individual's ID, name, department, and salary: " << endl; 
+    cin >> ID >> name >> department >> salary; 
+
+    
+
+  }
+  
+
+
+
+
+
+
 }
 
 
