3da8840f6c0e17ac7b7dd35846663d24ccb57564
Irene Kim <ikim5569@gmail.com>
2021-03-21 22:33:35 +0000

    test question 3 second

--- test-3.cpp
+++ test-3.cpp
@@ -3,13 +3,30 @@ using namespace std;
 
 int main(){
 
-  int range1, range2; 
+  int range1, range2, flag; 
 
   cout << "Please enter two values for the range(inclusive). First value should be smaller than second." << endl; 
 
   cin >> range1 >> range2; 
   
-  if 
+  if (range 1 >= range2) {
+    cout << "Please enter values again: " << endl; 
+    cin >> num1 >> num2; 
+  }
+  flag = 0; 
+
+  for (int i = range1; i <= range2; i++){
+    for (int j = 2; j < i; j++){
+      if(num/j == 0){
+        flag = 1; 
+        break; 
+      }
+      if (flag == 0) {
+        cout << i << "is prime" << endl; 
+      }
+    }
+  }
+
 
 
   return 0; 
