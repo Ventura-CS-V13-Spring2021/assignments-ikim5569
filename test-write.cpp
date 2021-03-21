a7daa595e3e0b0f75645f7a5925184db9b134793
Irene Kim <ikim5569@gmail.com>
2021-03-21 23:22:09 +0000

    test write 4

--- test-write.cpp
+++ test-write.cpp
@@ -4,7 +4,7 @@
 
 using namespace std; 
 
-int maint(){
+int main(){
 
   int num, ID; 
   string name, department; 
@@ -17,15 +17,27 @@ int maint(){
   cout << "How many employees? " << endl; 
   cin >> num; 
 
-  
+  write << num << endl; 
+
 
   for (int i = 0; i < num; i++){
-    cout << "Please enter the individual's ID, name, department, and salary: " << endl; 
-    cin >> ID >> name >> department >> salary; 
+    cout << "Please enter the individual's ID, name, department, and salary:  " << endl; 
 
-    
+    cin >> ID >> name >> department >> salary; 
+    write << ID; 
+    write << name; 
+    write << department; 
+    write << salary; 
+    write << endl; 
+
+   
+
+
 
   }
+
+  write.close(); 
+  return 0; 
   
 
 
