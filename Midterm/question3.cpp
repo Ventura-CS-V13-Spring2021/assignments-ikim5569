10dfb14ea74792a2e10d4c226e165814fa517026
Irene Kim <>
2021-04-18 21:30:59 +0000

    question 3

--- Midterm/question3.cpp
+++ Midterm/question3.cpp
@@ -47,22 +47,23 @@ int getRdNum(void){
 
 int findMin(int num1, int num2, int num3){
   int min; 
-  if ((num1 < num2) && (num1 < num3))
+  if ((num1 <= num2) && (num1 <= num3)){
     min = num1; 
-  if ((num2 < num1) && (num2 < num3))
-    min = num2; 
-  if ((num3 < num1) && (num3 < num2))
+  } else if ((num2 <= num1) && (num2 <= num3)){
+    min = num2;
+  } else if ((num3 <= num1) && (num3 <= num2)) {
     min = num3; 
+  }
   return min; 
 }
 
 int findMax(int num1, int num2, int num3){
   int max; 
-  if ((num1 > num2) && (num1 > num3))
+  if ((num1 >= num2) && (num1 >= num3))
     max = num1; 
-  if ((num2 > num1) && (num2 > num3))
+  if ((num2 >= num1) && (num2 >= num3))
     max = num2; 
-  if ((num3 > num1) && (num3 > num2))
+  if ((num3 >= num1) && (num3 >= num2))
     max = num3; 
   return max; 
 }
