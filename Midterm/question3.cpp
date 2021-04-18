b6415bf3adaab2eb9bc256e4ee3a6e29ec0eb5c4
Irene Kim <>
2021-04-18 21:25:24 +0000

    question 3

--- Midterm/question3.cpp
+++ Midterm/question3.cpp
@@ -21,13 +21,23 @@ int main(){
     num2 = getRdNum(); 
     num3 = getRdNum(); 
 
+    cout << "num1: " << num1 << endl; 
+    cout << "num2: " << num2 << endl; 
+    cout << "num3: " << num3 << endl; 
+
     min = findMin(num1, num2, num3); 
     max = findMax(num1, num2, num3); 
 
+    cout << "min: " << min << " and max: " << max << endl; 
+
     difference = getDifference(min, max); 
+
+    cout << "difference: " << difference<< endl; 
 
     write << difference << endl; 
   } while (difference >= 3); 
+
+  write.close(); 
 }
 
 int getRdNum(void){
@@ -45,4 +55,21 @@ int findMin(int num1, int num2, int num3){
     min = num3; 
   return min; 
 }
+
+int findMax(int num1, int num2, int num3){
+  int max; 
+  if ((num1 > num2) && (num1 > num3))
+    max = num1; 
+  if ((num2 > num1) && (num2 > num3))
+    max = num2; 
+  if ((num3 > num1) && (num3 > num2))
+    max = num3; 
+  return max; 
+}
+
+int getDifference(int min, int max){
+  int difference;  
+  difference = max - min; 
+  return difference; 
+} 
 
