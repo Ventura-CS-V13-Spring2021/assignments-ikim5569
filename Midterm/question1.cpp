9bb9a47f0e7927ab7ffcbd23f8715ec8af711032
Irene Kim <>
2021-04-18 20:15:19 +0000

    question 1 start

--- Midterm/question1.cpp
+++ Midterm/question1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
 #include <fstream>
-
+#include <string> 
 using namespace std;
+
+int main(){
+  string name; 
+  int test1, test2;   
+  ifstream read; 
+  read.open("students.txt"); 
+
+  while (read >> name >> test1 >> test2){
+    
+  }
+
+}
 
