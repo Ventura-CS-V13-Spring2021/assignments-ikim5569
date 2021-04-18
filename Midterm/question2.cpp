05f9122388375f7a9e8e67f70e597009988a4d4b
Irene Kim <>
2021-04-18 20:34:20 +0000

    question 2

--- Midterm/question2.cpp
+++ Midterm/question2.cpp
@@ -2,3 +2,26 @@
 #include <fstream>
 using namespace std;
 
+int getRdNum(void); 
+int isGreater(int); 
+
+int main(){
+  int preceding, num, N,i; 
+  preceding = 50; 
+  ofstream write; 
+  write.open("numbers.txt"); 
+
+  cout << "Please enter the number of random numbers: " << endl; 
+  cin >> N; 
+
+  for(i = 0; i < N; i++){
+    num = getRdNum(); 
+    if (isGreater(num)){
+      write << num << endl; 
+    }
+  }
+ write.close(); 
+}
+
+int getRdNum(void); 
+int isGreater(int); 
