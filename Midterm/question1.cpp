5129ce4afd29a80662e175804d92229a406d2303
Irene Kim <>
2021-04-18 20:18:17 +0000

    question 1 second push

--- Midterm/question1.cpp
+++ Midterm/question1.cpp
@@ -5,12 +5,26 @@ using namespace std;
 
 int main(){
   string name; 
-  int test1, test2;   
+  int test1, test2, greater;  
+  greater = 0;  
+  double average; 
+  double sum = 0; 
+
   ifstream read; 
   read.open("students.txt"); 
 
   while (read >> name >> test1 >> test2){
-    
+    sum = test1 + test2; 
+    average = sum/2; 
+    if (average > 80){
+      greater++; 
+    }
+
+    cout << "Student name: " << name; 
+    cout << " score 1: " << test1; 
+    cout << " score 2: " << test2; 
+    cout << " average: " << average << endl; 
+
   }
 
 }
