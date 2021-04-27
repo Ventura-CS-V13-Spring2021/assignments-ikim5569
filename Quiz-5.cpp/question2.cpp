a3ac4e4beda02c8741cb46bc08942b9fdf1f8225
Irene Kim <>
2021-04-27 02:12:21 +0000

    question 2: spacing issues

--- Quiz-5.cpp/question2.cpp
+++ Quiz-5.cpp/question2.cpp
@@ -39,22 +39,27 @@ void printarray(int num[], int size)
 void findmatchnum(int num1[], int size1, int num2[], int size2)
 {
   int match = 0;
-  bool repeat;  
-  for (int i = 0; i < size2; i++){
-    for (int j = 0; j < size1; j++){
+  bool repeat;
+
+  for (int i = 0; i < size2; i++)
+  {
+    for (int j = 0; j < size1; j++)
+    {
       repeat = false; 
-      for (int k = (i-1); k >= 0; k--) {
-        if (num2[i] == num2[k]) {
+      for (int k = (i-1); k >= 0; k--) 
+      {
+        if (num2[i] == num2[k]) 
+        {
           repeat = true; 
           break; 
         }
       }
       
-      if (repeat) {
+      if (repeat)
+      {
         break; 
       }
   
-      
       if ( num2[i] == num1[j]){
         match++; 
         break; 
