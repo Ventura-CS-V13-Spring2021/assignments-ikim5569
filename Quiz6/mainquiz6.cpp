a361dd80787bc340e693cc149a0ed4cd50179230
Irene Kim <>
2021-05-03 18:10:20 +0000

    final quiz 6

--- Quiz6/mainquiz6.cpp
+++ Quiz6/mainquiz6.cpp
@@ -14,13 +14,13 @@ int main()
 
     cout << "After sorting with ascending order : \n ";
 
-    N.sortArray(1);
+    N.sortArray(0);
 
     N.printAll();
 
     cout << "After sorting with descending order: \n ";
 
-    N.sortArray(0);
+    N.sortArray(1);
 
     N.printAll();
 
