f41550dd28ec7ace726deac51b07ccf6b4d3f304
Irene Kim <>
2021-05-15 21:16:28 +0000

    final

--- final/question1.cpp
+++ final/question1.cpp
@@ -8,40 +8,15 @@ int main()
 {
   int highestIndex; 
   int arr[6] = {1,2,8,11,13, 23};
-  int second[6] = {10,20,21,23,41}; 
+  int second[5] = {10,20,21,23,41}; 
 
-  int elementWithMost;  
-  int highestnumFactors = 0;  
-  int numFactors; 
-
-  for (int i = 0; i < 6; i++)
-  {
-    numFactors = 0; 
-    for (int j = 1; j < arr[i]; j++)
-    {
-      if (arr[i]%j == 0)
-      {
-        numFactors++; 
-        cout << "num factors for" << arr[i] << " is " << numFactors << endl; 
-
-      }
-    }
-    if (numFactors > highestnumFactors)
-    {
-      highestnumFactors = numFactors; 
-      elementWithMost = i; 
-
-    }
-  }
-
-  cout << "element with most factors is: " << elementWithMost << " which is " << arr[elementWithMost] << endl; 
   
 
-  // highestIndex = getNumDiv(arr, 6); 
-  // cout << "Index with most factors in arr is " << highestIndex << endl; 
+  highestIndex = getNumDiv(arr, 6); 
+  cout << "Index with most factors in arr is " << highestIndex << endl; 
 
-  // highestIndex = getNumDiv(second, 5); 
-  // cout << "Index with most factors in array second is " << highestIndex << endl; 
+  highestIndex = getNumDiv(second, 5); 
+  cout << "Index with most factors in array second is " << highestIndex << endl; 
 
 
   return 0; 
@@ -52,23 +27,29 @@ int main()
 
 int getNumDiv(int a[], int elements)
 {
-  int elementWithMost; 
-  int highestnumelements; 
-  int numElements; 
+  int elementWithMost;  
+  int highestnumFactors = 0;  
+  int numFactors; 
 
   for (int i = 0; i < elements; i++)
   {
-    numElements = 0; 
+    numFactors = 0; 
     for (int j = 1; j < a[i]; j++)
     {
       if (a[i]%j == 0)
-        numElements++; 
+      {
+        numFactors++; 
+        cout << "num factors for" << a[i] << " is " << numFactors << endl; 
+
+      }
     }
-    if (numElements > highestnumelements)
-      highestnumelements = numElements; 
+    if (numFactors > highestnumFactors)
+    {
+      highestnumFactors = numFactors; 
       elementWithMost = i; 
+
+    }
   }
-  return elementWithMost; 
 } 
 
 
